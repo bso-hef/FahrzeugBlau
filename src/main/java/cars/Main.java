d626ee6dd44149f1ccd4ba2be5e155573e0c9f0b
fdai2003 <fdai2003@gmail.com>
2024-02-23 16:15:14 +0100

    Initial commit

---
+++ src/main/java/cars/Main.java
@@ -0,0 +1,11 @@
+package cars;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        Fahrzeug1Blau fahrzeugBlau = new Fahrzeug1Blau();
+        fahrzeugBlau.vorwärtsFahren(2000);
+
+    }
+
+}
