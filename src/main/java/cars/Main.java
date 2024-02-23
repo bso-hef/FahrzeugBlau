863d4697431b71420080a6818defac5baba35f66
fdai2003 <fdai2003@gmail.com>
2024-02-23 19:16:37 +0100

    final modification for initial use

--- src/main/java/cars/Main.java
+++ src/main/java/cars/Main.java
@@ -3,8 +3,8 @@ package cars;
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        Fahrzeug1Blau fahrzeugBlau = new Fahrzeug1Blau();
-        fahrzeugBlau.vorwärtsFahren(2000);
+        Fahrzeug1Blau fahrzeug = new Fahrzeug1Blau();
+        fahrzeug.vorwärtsFahren(20);
 
     }
 
